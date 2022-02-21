054aa55c98112855405cca65e8b27014c2b186f6
Ico no Yuri <iconoyuri@gmail.com>
2022-02-21 06:47:21 +0100

    Router setting + Authentication via login

--- accounts/router.py
+++ accounts/router.py
@@ -1,30 +1,43 @@
-from fastapi import FastAPI, status, Depends, Response, HTTPException
+from fastapi import APIRouter, status, Depends, Response, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from starlette.responses import RedirectResponse
 from accounts.api_classes.login_form import LoginForm
 from accounts.api_classes.registration_form import RegistrationForm
-from accounts.db_driver import DBDriver
 from accounts.login.login_handler import LoginHandler
 from accounts.registration.registration_handler import RegistrationHandler
+from accounts import token
 
-# prefix : /account
-app = FastAPI()
+router = APIRouter(
+    prefix="/account",
+    tags= ["Authentication"]
+)
 
-@app.post("/login",status_code=status.HTTP_302_FOUND)
-def login(login_form: LoginForm):
+@router.post("/login",status_code=status.HTTP_200_OK)
+# def login(login_form: LoginForm):
+def login(login_form: OAuth2PasswordRequestForm = Depends()):   
     
-    user = LoginHandler.user_log_in(login_form)
-    return user
+    # user = LoginHandler.user_log_in(login_form)
+    user = LoginHandler.user_log_in(
+            LoginForm(
+                identifier=login_form.username,
+                password=login_form.password  
+                      )
+        )
+    # return user
+    
+    access_token = token.create_access_token(
+        data={"sub": user.mail}
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
 
-@app.post("/register",status_code=status.HTTP_201_CREATED)
+@router.post("/register",status_code=status.HTTP_201_CREATED)
 def register(registration_form: RegistrationForm):
     
     RegistrationHandler.user_registration(registration_form)
-    response = RedirectResponse(url='/account/activate')
+    response = RedirectResponse(url='http://localhost:8000/activate')
     return response
 
-@app.post("/activate/{regist_code}",status_code=status.HTTP_201_CREATED)
+@router.post("/activate/{regist_code}",status_code=status.HTTP_202_ACCEPTED)
 def activate(regist_code):
+    
     RegistrationHandler.activate_account(regist_code)
-    ...
-    # user = LoginHandler.user_log_in(login_form)
-    # return user
--- main.py
+++ main.py
@@ -1,14 +1,30 @@
-from fastapi import FastAPI
+from accounts import token
+from fastapi import FastAPI, Depends, status
+from fastapi.security import OAuth2PasswordRequestForm
+from starlette.responses import  RedirectResponse
+from accounts.api_classes.login_form import LoginForm
+from accounts.data_classes.user import User
+from accounts.login.login_handler import LoginHandler
+from accounts.oauth2 import get_current_user
+import accounts.router as accounts_router
 
-app = FastAPI()
+app = FastAPI() 
+app.include_router(accounts_router.router)
 
-@app.get("/")
-def index():
+
+@app.get("/test")
+def index(current_user: User = Depends(get_current_user)):
     return {
         "data" : "Hello World"
     }
     
-# There is two databases for the users login
+@app.get("/redirect")
+async def redirect():
+    response = RedirectResponse(url='/redirected')
+    return response
+
+
+# There are two databases for the users login
 # - A permanent database for the users whom are recognized as members of the site. They are the users whom have activated
 # their account
 # - A temporary database for the users whom have sent their datas, but haven't yet activated it
