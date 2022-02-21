91d0f77c9d7c75ff49828523ea76c42d35580ed5
Ico no Yuri <iconoyuri@gmail.com>
2022-02-21 06:50:39 +0100

    functions refactoring and defining (tests left)

--- accounts/activation/account_activation_handler.py
+++ accounts/activation/account_activation_handler.py
@@ -1,15 +1,44 @@
+from accounts.communication.mail_communicator import MailCommunicator
 from accounts.data_classes.user import User
+from fastapi import FastAPI
+from fastapi_mail import FastMail, MessageSchema,ConnectionConfig
+from starlette.requests import Request
+from starlette.responses import JSONResponse
+from pydantic import EmailStr, BaseModel
+from typing import List
 
 
 class AccountActivationHandler:
     
     @classmethod
-    def send_activation_mail(self, user: User) -> bool:
-        link = self.generate_activation_link(user)
-        ...
-        
+    def send_activation_mail(self, user: User, activation_code : int) -> bool:
+        mail_content = self.generate_activation_mail(user, activation_code)
+        message = MessageSchema(
+            subject="[Youtube Dev] Your activation code",
+            recipients=[user.mail], # List of recipients, as many as you can pass
+            body=mail_content,
+            subtype="html"
+        )
+        MailCommunicator.send_activation_mail(message)
+
     @classmethod
-    def generate_activation_link(self, user) -> str: 
-        # This function generates an activation link 
-        ...
+    def generate_activation_mail(self, user:User, activation_code : str) :
+        # return the mail created
         
+        template = f"""
+            <html>
+            <body>
+            
+
+            <p>Hello {user.login}
+            <br>This is your youtube dev account activation code</p>
+            
+            <h1>{activation_code}</h1>
+
+
+            </body>
+            </html>
+		"""
+        return template
+    
+    
--- accounts/communication/mail_communicator.py
+++ accounts/communication/mail_communicator.py
@@ -1,21 +1,27 @@
+from fastapi_mail import FastMail, MessageSchema,ConnectionConfig
+from pydantic import EmailStr, BaseModel
+from typing import List
 
 
-from accounts.data_classes.user import User
 
+# class EmailSchema(BaseModel):
+#    email: List[EmailStr]
+   
 
+   
 class MailCommunicator: 
+    conf = ConnectionConfig(
+        MAIL_USERNAME="ytbdev10@gmail.com",
+        MAIL_PASSWORD="Ycono10@2222",
+        MAIL_PORT=587,
+        MAIL_SERVER="smtp.gmail.com",
+        MAIL_TLS=True,
+        MAIL_SSL=False
+        )
     
     @classmethod
-    def send_activation_mail(self, email: User) -> bool:
+    async def send_activation_mail(self, message: MessageSchema) -> None:
         # create the activation link and send it to user's mail address
-        ...
+        fm = FastMail(self.conf)
+        await fm.send_message(message)
         
-    @classmethod
-    def generate_activation_mail(self, link:str, address : str) :
-        # return the mail created
-        ...
-        
-    @classmethod
-    def generate_activation_link(self, user : User) :
-        # return the mail created
-        ...
