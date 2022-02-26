464174c9b82570d7f12b405d53defee5a3bf86ef
Ico no Yuri <iconoyuri@gmail.com>
2022-02-26 10:08:33 +0100

    useless comments deletion

--- accounts/router.py
+++ accounts/router.py
@@ -4,7 +4,7 @@ from starlette.responses import RedirectResponse
 from accounts.api_classes.login_form import LoginForm
 from accounts.api_classes.registration_form import RegistrationForm
 from accounts.login.login_handler import LoginHandler
-from accounts.registration.registration_handler import RegistrationHandler
+from accounts.login.registration_handler import RegistrationHandler
 from accounts import token
 
 router = APIRouter(
@@ -13,17 +13,14 @@ router = APIRouter(
 )
 
 @router.post("/login",status_code=status.HTTP_200_OK)
-# def login(login_form: LoginForm):
 def login(login_form: OAuth2PasswordRequestForm = Depends()):   
     
-    # user = LoginHandler.user_log_in(login_form)
     user = LoginHandler.user_log_in(
             LoginForm(
                 identifier=login_form.username,
                 password=login_form.password  
                       )
         )
-    # return user
     
     access_token = token.create_access_token(
         data={"sub": user.mail}
@@ -34,10 +31,13 @@ def login(login_form: OAuth2PasswordRequestForm = Depends()):
 def register(registration_form: RegistrationForm):
     
     RegistrationHandler.user_registration(registration_form)
-    response = RedirectResponse(url='http://localhost:8000/activate')
+    response = RedirectResponse(url='http://localhost:8000/account/activate')
     return response
 
 @router.post("/activate/{regist_code}",status_code=status.HTTP_202_ACCEPTED)
 def activate(regist_code):
     
     RegistrationHandler.activate_account(regist_code)
+    return {
+        "message" : "Activation successful"
+    }
