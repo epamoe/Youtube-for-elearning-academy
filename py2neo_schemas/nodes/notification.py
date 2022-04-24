e4ce14e8e4b13da81c0639f3fd91f982ae9a1b16
icono yuri <iconoyuri@gmail.com>
2022-04-24 18:29:59 +0100

    new member application text variable adding

--- py2neo_schemas/nodes/notification.py
+++ py2neo_schemas/nodes/notification.py
@@ -2,8 +2,14 @@ from py2neo.ogm import Property,RelatedFrom
 from py2neo_schemas.nodes.root_graph_object import RootGraphObject
 
 class Notification(RootGraphObject):
+
+    NEW_APPLICATION_TEXT = "Congratulations ! You applied to become a member. An expert will answer to your request"
     
     content = Property()
-    status = Property()
+    read = Property()
 
     user_notified = RelatedFrom("User","NOTIFY")
+
+    def __init__(self, *values, **properties):
+        super().__init__(*values, **properties)
+        self.read = False
