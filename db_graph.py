b245d99db1853acde3a407848e901fc1d2dcf501
icono yuri <iconoyuri@gmail.com>
2022-04-25 12:27:36 +0100

    connection failure exception handled

--- db_graph.py
+++ db_graph.py
@@ -1,5 +1,6 @@
 import os
 from dotenv import load_dotenv
+from fastapi import HTTPException,status
 
 from py2neo import Graph
 
@@ -7,4 +8,10 @@ load_dotenv()
 GDB_URI = os.getenv("GDB_URI")
 GDB_USERNAME = os.getenv("GDB_USERNAME")
 GDB_PASSWORD = os.getenv("GDB_PASSWORD")
-graph = Graph(uri=GDB_URI,auth=(GDB_USERNAME,GDB_PASSWORD))
+try:
+    graph = Graph(uri=GDB_URI,auth=(GDB_USERNAME,GDB_PASSWORD))
+except Exception:
+    raise HTTPException(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        detail="The connection to the database failed"
+    )
