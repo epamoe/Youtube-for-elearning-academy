1b87516c1ec9da6c37ac6e6dc12488a1a470e0c2
icono yuri <iconoyuri@gmail.com>
2022-04-27 13:14:06 +0100

    analytics history and presence apis setting

--- routers/analytics.py
+++ routers/analytics.py
@@ -1,4 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from oauth2 import get_current_user
+from py2neo_schemas.nodes import Page, SearchInput, User
+from db_graph import graph
 import schemas
 router = APIRouter(
     prefix = "/analytics",
@@ -6,12 +9,16 @@ router = APIRouter(
 )
 
 @router.post("/search/history")
-def search(search: schemas.AnalyticsSearch):
-    return search
+def search(search: str, user_login = Depends(get_current_user)):
+    user = User.match(graph, user_login).first()
+    search_input = SearchInput(content= search)
+    user.search.add(search_input)
+    graph.push(user)
 
 @router.post("/presence")
-def presence(page: schemas.AnalyticsPresence):
-    return page
+def presence(page: str, user_login = Depends(get_current_user)):
+    graph.run("MATCH (u:User{login:'"+user_login+"'}) MERGE (u)-[v:VISIT]->(p:Page{content:'"+page+"'}) ON CREATE SET v.nbr = 1 ON MATCH SET v.nbr = v.nbr + 1 ")
+
 
 
 @router.get("/lesson/{id}")
