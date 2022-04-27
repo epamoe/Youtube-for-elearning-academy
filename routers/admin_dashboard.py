c1ee50c30cc7925365179f2afb825fcc69c3988d
icono yuri <iconoyuri@gmail.com>
2022-04-27 11:00:48 +0100

    member application api defining

--- routers/admin_dashboard.py
+++ routers/admin_dashboard.py
@@ -1,5 +1,7 @@
+from typing import List
 from fastapi import APIRouter, Depends, HTTPException,status
 from py2neo_schemas.nodes import User, Application
+from schemas import ApplicationResponse
 
 from oauth2 import get_current_user
 from db_graph import graph
@@ -36,6 +38,16 @@ def validate(application_id: int, user_login = Depends(get_current_user)):
             detail="This application is already treated"
         )
 
-@router.get("/member_application/")
-def validate():
+@router.get("/member_application/", response_model=List[ApplicationResponse])
+def get_applications(user_login = Depends(get_current_user)):
+    user = User.match(graph,user_login).first()
+    if not user.admin:
+        raise HTTPException(
+            status_code= status.HTTP_401_UNAUTHORIZED,
+            detail="You need admin rights to realize that operation"
+        )
+
+    applications =  list(Application.match(graph).where("_.status = '" + Application.PENDING + "'"))
+    apps_users = [Application(status = app.status, user_login = list(app.candidates)[0].login) for app in applications]
+    return apps_users
     ...
