cf2fbb33acede71e62230568af61df18338f08d9
icono yuri <iconoyuri@gmail.com>
2022-05-02 00:15:09 +0100

    implementing lesson and video watch analytics APIs

--- routers/analytics.py
+++ routers/analytics.py
@@ -19,13 +19,13 @@ def presence(page: str, user_login = Depends(get_current_user)):
     graph.run("MATCH (u:User{login:'"+user_login+"'}) MERGE (u)-[v:VISIT]->(p:Page{content:'"+page+"'}) ON CREATE SET v.nbr = 1 ON MATCH SET v.nbr = v.nbr + 1 ")
 
 
-
 @router.get("/lesson/{uuid}")
-def analytic_lesson(uuid:int ,user_login = Depends(get_current_user)):
-    graph.run("MATCH (u:User{login:'"+user_login+"'}) MERGE (u)-[c:WATCH]->(l:Lesson{uuid:"+str(uuid)+"}) ON CREATE SET l.nbr = 1 ON MATCH SET l.nbr = l.nbr + 1 ")
+def analytic_lesson(uuid:str ,user_login = Depends(get_current_user)):
+    graph.run("MATCH (u:User{login:'"+user_login+"'}),(l:Lesson{uuid:'"+str(uuid)+"'}) MERGE (u)-[c:WATCH]->(l) ON CREATE SET c.nbr = 1 ON MATCH SET c.nbr = c.nbr + 1 ")
 
 
 @router.get("/video/{video_id}")
-def analytic_video(video_id:str ,user_login = Depends(get_current_user)):
+def analytic_video(uuid:str ,user_login = Depends(get_current_user)):
+    graph.run("MATCH (u:User{login:'"+user_login+"'}),(v:Video{uuid:'"+str(uuid)+"'}) MERGE (u)-[c:WATCH]->(v) ON CREATE SET c.nbr = 1 ON MATCH SET c.nbr = c.nbr + 1 ")
     ...
     
