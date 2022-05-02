e06d681398c619db67f33a90718a808edeb3e5aa
icono yuri <iconoyuri@gmail.com>
2022-05-02 12:46:01 +0100

    queries refactoring

--- routers/analytics.py
+++ routers/analytics.py
@@ -16,16 +16,43 @@ def search(search: str, user_login = Depends(get_current_user)):
 
 @router.post("/presence")
 def presence(page: str, user_login = Depends(get_current_user)):
-    graph.run("MATCH (u:User{login:'"+user_login+"'}) MERGE (u)-[v:VISIT]->(p:Page{content:'"+page+"'}) ON CREATE SET v.nbr = 1 ON MATCH SET v.nbr = v.nbr + 1 ")
+    query = """
+        MATCH (u:User{login:$user_login}) 
+        MERGE (u)-[v:VISIT]->(p:Page{page:$page}) 
+        ON CREATE SET v.nbr = 1 ON MATCH SET v.nbr = v.nbr + 1 """
+    params = {
+        "user_login" : user_login, 
+        "page": page
+    }
+    graph.run(query, params)
 
 
 @router.get("/lesson/{uuid}")
 def analytic_lesson(uuid:str ,user_login = Depends(get_current_user)):
-    graph.run("MATCH (u:User{login:'"+user_login+"'}),(l:Lesson{uuid:'"+str(uuid)+"'}) MERGE (u)-[c:WATCH]->(l) ON CREATE SET c.nbr = 1 ON MATCH SET c.nbr = c.nbr + 1 ")
+    query = """
+        MATCH (u:User{login:$user_login}),(l:Lesson{uuid:$uuid}) 
+        MERGE (u)-[c:WATCH]->(l) 
+        ON CREATE SET c.nbr = 1 ON MATCH SET c.nbr = c.nbr + 1 
+    """
+    params = {
+        "user_login" : user_login,
+        "uuid" : uuid
+    }
+    graph.run(query, params)
 
 
 @router.get("/video/{video_id}")
 def analytic_video(uuid:str ,user_login = Depends(get_current_user)):
-    graph.run("MATCH (u:User{login:'"+user_login+"'}),(v:Video{uuid:'"+str(uuid)+"'}) MERGE (u)-[c:WATCH]->(v) ON CREATE SET c.nbr = 1 ON MATCH SET c.nbr = c.nbr + 1 ")
+    query = """
+        MATCH (u:User{login:$user_login}),(v:Video{uuid:$uuid}) 
+        MERGE (u)-[c:WATCH]->(v) 
+        ON CREATE SET c.nbr = 1 
+        ON MATCH SET c.nbr = c.nbr + 1 "
+    """
+    params = {
+        "user_login" : user_login,
+        "uuid" : uuid
+    }
+    graph.run(query, params)
     ...
     
